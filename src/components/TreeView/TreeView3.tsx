d7200cc262af9f99dffa8fe4eae4b7d151300bb7
robert <robert@elloco.com>
2025-04-02 16:42:57 -0400

    se arreglo el espacio de elementos en el nivel 0, ahora todos tienen el mismo margen

--- src/components/TreeView/TreeView3.tsx
+++ src/components/TreeView/TreeView3.tsx
@@ -1,6 +1,7 @@
 import { IPWARoutes } from "faster-router";
 import "./style.css";
 import {
+  Box,
   Collapse,
   ListItemButton,
   ListItemIcon,
@@ -18,101 +19,58 @@ interface TreeViewProps3 {
   level?: number;
 }
 
-const TreeView3: React.FC<TreeViewProps3> = ({
-  routes,
-  parent = "",
-  level = 0,
-}) => {
+const TreeView3: React.FC<TreeViewProps3> = ({ routes, parent = "", level = 0 }) => {
   const { open, handleClick } = useTreeViewLogic();
-  const hasParent = !!parent;
   const navigate = useNavigate();
+  const hasParent = Boolean(parent);
+
   return (
     <Stack component="nav" sx={{ color: "text.secondary", padding: 0 }}>
-      <ul
-        className="treeUl"
-        style={{
-          position: "relative",
-          borderLeft: hasParent ? "2px solid red" : "none",
-          marginLeft: hasParent ? 16 : 0,
-          paddingLeft: hasParent ? 16 : 0,
-        }}
-      >
+      <ul className="treeUl" style={{ position: "relative" }}>
         {Object.values(routes)
           .filter(({ title }) => title)
-          .map((item, index) => {
-            const { subPath, path, title, icon: Icon } = item;
-            const fullPath = `${parent}${item.path}`;
+          .map(({ subPath, path, title, icon: Icon }, index, array) => {
+            const fullPath = `${parent}${path}`;
+            const last = index === array.length - 1;
+            const first = index === 0;
+
             console.log(fullPath);
-            if (hasParent) {
-              console.log(title, hasParent, fullPath, level);
-            }
+            if (hasParent) console.log(title, hasParent, fullPath, level);
 
             return (
               <Fragment key={path}>
-                {!subPath ? (
-                  <>
-                    <li
-                      key={path || index}
-                      className="treeLi"
-                      style={{ position: "relative" }}
-                    >
-                      {hasParent && (
-                        <div
-                          style={{
-                            position: "absolute",
-                            left: -16,
-                            top: "50%",
-                            width: 16,
-                            height: 2,
-                            backgroundColor: "red",
-                            transform: "translateY(-50%)",
-                          }}
-                        />
-                      )}
-                      <ListItemButton
-                        onClick={() => navigate(fullPath)}
-                        sx={{ pl: hasParent && !(level > 0) ? 4 : 2 }}
-                      >
-                        {Icon && (
-                          <ListItemIcon>
-                            <Icon />
-                          </ListItemIcon>
-                        )}
-                        <ListItemText primary={title} />
-                      </ListItemButton>
-                    </li>
-                  </>
-                ) : (
-                  <>
-                    <li key={path || index} style={{ position: "relative" }}>
-                      <ListItemButton
-                        onClick={() => handleClick(title as string)}
-                        sx={{}}
-                      >
-                        {Icon && (
-                          <ListItemIcon>
-                            <Icon />
-                          </ListItemIcon>
-                        )}
-                        <ListItemText primary={title} />
-                        {open[title as never] ? <ExpandLess /> : <ExpandMore />}
-                      </ListItemButton>
-
-                      <Collapse
-                        in={open[title as never]}
-                        timeout="auto"
-                        unmountOnExit
-                        sx={{ px: 2, paddingRight: 0 }}
-                      >
-                        <TreeView3
-                          routes={subPath}
-                          parent={`${parent}${path}/`}
-                          level={level + 1}
-                        />
-                      </Collapse>
-                    </li>
-                  </>
-                )}
+                <li className="treeLi" style={{ position: "relative" }}>
+                  <ListItemButton
+                    onClick={() => (subPath ? handleClick(title as string) : navigate(fullPath))}
+                    sx={{ ml: hasParent && level === 0 ? 4 : 2 }}
+                  >
+                    {hasParent && (
+                      <Box
+                        sx={{
+                          borderLeft: "2px solid green",
+                          borderRadius: last ? "0 0 0 50%" : "none",
+                          borderBottom: "2px solid green",
+                          width: "10px",
+                          height: first ? "50%" : "100%",
+                          transform: "translate(-160%, -50%)",
+                          position: "absolute",
+                        }}
+                      />
+                    )}
+                    {Icon && (
+                      <ListItemIcon>
+                        <Icon />
+                      </ListItemIcon>
+                    )}
+                    <ListItemText primary={title} />
+                    {subPath && (open[title as never] ? <ExpandLess /> : <ExpandMore />)}
+                  </ListItemButton>
+                  {subPath && (
+                    <Collapse in={open[title as never]} timeout="auto" unmountOnExit sx={{ px: 2, paddingRight: 0 }}>
+                      <TreeView3 routes={subPath} parent={`${parent}${path}/`} level={level + 1} />
+                    </Collapse>
+                  )}
+                </li>
               </Fragment>
             );
           })}
